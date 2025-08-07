ae6ab9f66e84c5faeffaa7b1b67accd18de9a049
Dima Bolnik <dimabolnik@mail.ru>
2025-08-07 11:08:00 +0300

    Добавление Eureka-Server

---
+++ eureka-server/src/main/java/ru/bolnik/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package ru.bolnik.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
