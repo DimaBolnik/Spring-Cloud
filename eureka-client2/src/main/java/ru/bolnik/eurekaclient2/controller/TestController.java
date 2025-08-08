b71844d88fad51de67fdc3bd3cb6c1884a497dde
Dima Bolnik <dimabolnik@mail.ru>
2025-08-08 11:00:25 +0300

    проверка балансера и добавление нового микросервиса

---
+++ eureka-client2/src/main/java/ru/bolnik/eurekaclient2/controller/TestController.java
@@ -0,0 +1,19 @@
+package ru.bolnik.eurekaclient2.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/main2")
+public class TestController {
+
+    @Value("${eureka.instance.instance-id}")
+    private String instanceId;
+
+    @GetMapping("/test2")
+    public String test() {
+        return instanceId;
+    }
+}
